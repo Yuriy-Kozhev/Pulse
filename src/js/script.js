1371eb7f2acadff71d5b6024c92efc74ff00aa36
Rick <lifespace@ya.ru>
2023-11-18 23:22:51 +0300

    PageUp and validation

--- src/js/script.js
+++ src/js/script.js
@@ -115,4 +115,20 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // Page-up
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".page-up").fadeIn();
+    } else {
+      $(".page-up").fadeOut();
+    }
+  });
+
+  $("a[href='#up']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
 });
